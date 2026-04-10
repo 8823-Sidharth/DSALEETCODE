c3845a61ca26dbb9de114f3b5b06363daaff9fc6
8823-Sidharth <sidharthakur0023@gmail.com>
2026-04-10 15:46:23 +0530

    Time: 3 ms (46.53%), Space: 9.9 MB (23.82%) - LeetHub

--- 0242-valid-anagram/0242-valid-anagram.cpp
+++ 0242-valid-anagram/0242-valid-anagram.cpp
@@ -2,28 +2,28 @@ class Solution {
 public:
     bool isAnagram(string s, string t) {
         if(s.size()!=t.size()) return false;
-        // map<char,int> freqs;
-        // map<char,int> freqt;
-        // for(char ch:s){
-        //     freqs[ch]++;
+        map<char,int> freqs;
+        map<char,int> freqt;
+        for(char ch:s){
+            freqs[ch]++;
+        }
+        for(char ch:t){
+            freqt[ch]++;
+        }
+        if(freqs==freqt) return true;
+        return false;
+        // map<char,int> freq;
+        // for(char c:s){
+        //     freq[c]++;
         // }
-        // for(char ch:t){
-        //     freqt[ch]++;
+        // for(char c:t){
+        //     freq[c]--;
         // }
-        // if(freqs==freqt) return true;
-        // return false;
-        map<char,int> freq;
-        for(char c:s){
-            freq[c]++;
-        }
-        for(char c:t){
-            freq[c]--;
-        }
-        for(auto it:freq){
-            if(it.second!=0){
-                return false;
-            }
-        }
-        return true;
+        // for(auto it:freq){
+        //     if(it.second!=0){
+        //         return false;
+        //     }
+        // }
+        // return true;
     }
 };
