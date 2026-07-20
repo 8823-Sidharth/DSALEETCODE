49e00f6d3aff444e6ac9f085d513a0adc0abb0d3
8823-Sidharth <sidharthakur0023@gmail.com>
2026-07-20 10:50:32 +0530

    Time: 0 ms (100%), Space: 18.1 MB (91.78%) - LeetHub

--- 1260-shift-2d-grid/1260-shift-2d-grid.cpp
+++ 1260-shift-2d-grid/1260-shift-2d-grid.cpp
@@ -1,25 +1,38 @@
 class Solution {
 public:
     vector<vector<int>> shiftGrid(vector<vector<int>>& grid, int k) {
-        int n=grid.size();
-        int m=grid[0].size();
-        while(k--){
-            vector<vector<int>> temp(n,vector<int> (m));
-            for(int i=0;i<n;i++){
-                for(int j=0;j<m;j++){
-                    if(i==n-1 && j==m-1){
-                        temp[0][0]=grid[i][j];
-                    }
-                    else if(j==m-1){
-                        temp[i+1][0]=grid[i][j];
-                    }
-                    else{
-                        temp[i][j+1]=grid[i][j];
-                    }
-                }
+        int m=grid.size();
+        int n=grid[0].size();
+        // while(k--){
+        //     vector<vector<int>> temp(m,vector<int> (n));
+        //     for(int i=0;i<n;i++){
+        //         for(int j=0;j<m;j++){
+        //             if(i==m-1 && j==n-1){
+        //                 temp[0][0]=grid[i][j];
+        //             }
+        //             else if(j==n-1){
+        //                 temp[i+1][0]=grid[i][j];
+        //             }
+        //             else{
+        //                 temp[i][j+1]=grid[i][j];
+        //             }
+        //         }
+        //     }
+        //     grid=temp;
+        // }
+        // return grid;
+        int total=m*n;
+        k=k%total;
+        vector<vector<int>> temp(m,vector<int> (n));
+        for(int i=0;i<m;i++){
+            for(int j=0;j<n;j++){
+                int currindex = i*n+j;
+                int newindex = (currindex+k)%total;
+                int newrow = newindex / n;
+                int newcol = newindex % n;
+                temp[newrow][newcol]=grid[i][j];
             }
-            grid=temp;
         }
-        return grid;
-    }
+    return temp;
+    }    
 };
